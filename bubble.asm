e808a652ed5d0f77fa5ace4eaf99bfa893739466
aln730 <arnsg730@proton.me>
2025-07-03 22:08:11 -0400

    IT WORKS

--- bubble.asm
+++ bubble.asm
@@ -7,22 +7,22 @@ section .text
 
 _start:
     mov edi, size           ; outer loop counter
-    dec edi                 ; runs from 0 to size-2
+    dec edi                 ; runs from 0 to size - 2
 
 outer_loop:
-    mov esi, 0              ; inner loop index j = 0
+    mov esi, 0              ; inner index = 0
     mov ecx, size
-    dec ecx                 ;
+    dec ecx                 ; inner loop runs size - 1 times
 
 inner_loop:
-    mov eax, [array + esi*4]       ; eax = array[j]
-    mov ebx, [array + esi*4 + 4]   ; ebx = array[j+1]
+    mov eax, [array + esi*4]           ; eax = array[esi]
+    mov ebx, [array + (esi+1)*4]       ; ebx = array[esi + 1]
     cmp eax, ebx
     jle no_swap
 
-    ; Swap array[j] and array[j+1]
+    ; Swap array[esi] and array[esi + 1]
     mov [array + esi*4], ebx
-    mov [array + esi*4 + 4], eax
+    mov [array + (esi+1)*4], eax
 
 no_swap:
     inc esi
@@ -33,4 +33,4 @@ no_swap:
     jnz outer_loop
 
 hang:
-    jmp hang       ; infinite loop
+    jmp hang
